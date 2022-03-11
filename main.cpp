06a51dd985b8494394c4aba6bdd6a15df9bb0494
ViliRajh <vili.rajh@student.um.si>
2022-03-11 19:44:07 +0100

    Dodan sprejem besedila

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 int main() {
     std::cout << "Prikaz demonstarcije dodajanja projekta na Github\n\n";
 
+    std::string SS;
+    std::cout <<"vnesi besedilo: ";
+    std::cin >> SS;
 
     return 0;
 }
